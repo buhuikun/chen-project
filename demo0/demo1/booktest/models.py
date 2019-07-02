404805038c4154914840f2c11577cbaed5356ddd
chen <2271992921@qq.com>
2019-07-02 15:38:15 +0800

    生成迁移文件 迁移

--- demo0/demo1/booktest/models.py
+++ demo0/demo1/booktest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    pub_date = models.DateTimeField(auto_now=True)
+
+class HeroInfo(models.Model):
+    name = models.CharField(max_length=50)
+    gender = models.BooleanField(default=True)
+    content = models.CharField(max_length=100)
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
--- demo0/demo1/demo1/settings.py
+++ demo0/demo1/demo1/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# 注册的应用
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booktest',
 ]
 
 MIDDLEWARE = [
