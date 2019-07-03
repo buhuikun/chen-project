56426c6bc40bc5ea253b15c922a1c5b9b770e390
chen <2271992921@qq.com>
2019-07-03 20:46:16 +0800

    完善demo2

--- demo0/demo2/booktest1/urls.py
+++ demo0/demo2/booktest1/urls.py
@@ -6,7 +6,7 @@ from . import views
 # 绑定路由
 urlpatterns = [
     # （URL， 绑定的视图函数）
-    url(r'^index$', views.index),
+    url(r'^index/$', views.index),
     url(r'^list/$', views.list),
     url(r'^detail/(\d+)/$', views.detail)
 ]
--- demo0/demo2/booktest1/views.py
+++ demo0/demo2/booktest1/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from .models import HeroInfo, BookInfo
 
 def index(request):
-    return render(request, 'booktest/index.html', {'username': 'chen'})
+    return render(request, 'booktest1/index.html', {'username': 'chen'})
 
 
 def list(request):
     books = BookInfo.objects.all()
-    return render(request, 'booktest/list.html', {'books': books})
+    return render(request, 'booktest1/list.html', {'books': books})
 
 
 def detail(request, id):
     book = BookInfo.objects.get(pk=id)
-    return render(request, 'booktest/detail.html', {'book': book})
+    return render(request, 'booktest1/detail.html', {'book': book})
